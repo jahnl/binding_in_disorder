60310b8e3f08d8d2b674905ff10fab46d6b77922
Laura <ge72fev@mytum.de>
2022-05-25 11:24:40 +0200

    adapted CV to the new labels, fixed wrong splits of special case proteins

--- src/CV_splits.py
+++ src/CV_splits.py
@@ -3,17 +3,16 @@ from sklearn.model_selection import KFold
 if __name__ == '__main__':
     # Input Training Data
     data_dir = '../dataset/'
-    with open(data_dir + "train_set_annotation.tsv", 'r') as train_set:
+    with open(data_dir + "train_set_input.txt", 'r') as train_set_labels:
         # Split Training Set for k-fold Cross Validation
         k_fold = KFold(n_splits=5, shuffle=True, random_state=707)
         # generate list of arrays of indices
-        train_set_lines = train_set.readlines()[1:]
-        subsets = [x[1] for x in k_fold.split(train_set_lines)]
+        label_lines = train_set_labels.readlines()
+        subsets = [x[1] for x in k_fold.split(label_lines)]
         # print(subsets)
         # for each subset write annotation to a new file
         for i in enumerate(subsets):
-            with open(data_dir + 'folds/' + f"CV_fold_{i[0]}.tsv", mode="w") as output:
-                output.write("ID\tsequence\tmax.Region.ID\tResidues\tAnnotations\tBindEmbed\n")
-                entries = [train_set_lines[x] for x in subsets[i[0]]]
+            with open(data_dir + 'folds/' + f"CV_fold_{i[0]}_labels.txt", mode="w") as output_labels:
+                entries = [''.join(label_lines[4*x:4*x+4]) for x in subsets[i[0]]]
                 for j in entries:
-                    output.write(j)
+                    output_labels.write(j)
