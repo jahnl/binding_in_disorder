eb993b6528aec0820bbbd903d23a0a431ae40668
Laura <ge72fev@mytum.de>
2022-07-13 18:16:54 +0200

    renaming models, removing model 2.2 with dropout 0.2 from final model selection

--- R_scripts/performance_viz.R
+++ R_scripts/performance_viz.R
@@ -14,12 +14,12 @@ scale_color_colorblind7 = function(.ColorList = 2L:9L, ...){
 
 
 performance <- data.table(read.table("../results/logs/performance_assessment.tsv", header = TRUE, sep = "\t"))
-performance <- cbind(performance, model_name = c("0 simple_CNN", "1 larger_CNN", "2 FNN", "2.1 oversampling", "2.2 dropout_0.2", "2.2 dropout_0.3", "2.2 dropout_0.3 post-processing", "random baseline",
+performance <- cbind(performance, model_name = c("0 simple_CNN", "1 larger_CNN", "2 FNN", "2.1 oversampling", "2.2 dropout_0.2", "2.2 dropout", "2.3 post-processing", "random baseline",
                                             "3 disorder_only", "random baseline", rep("4 multilabel", 3), rep("4.1 oversampling", 3), rep("random baseline", 3)))
 performance <- cbind(performance, class_name = c(rep("-", 10), rep(c("1: protein-binding", "2: nuc-binding", "3: 'other'-binding"), 3)))
 
 #binary predictors, all metrics
-ggplot(data = performance[1:8])+
+ggplot(data = rbind(performance[1:4], performance[6:8]))+
   geom_bar(mapping = aes(x = "Precision", y = Precision, fill = model_name), stat = "identity", position = position_dodge2())+
   geom_errorbar(mapping = aes(x = "Precision", ymin = Precision - SE_Precision, ymax = Precision + SE_Precision), position = position_dodge2())+
   geom_bar(mapping = aes(x = "Recall", y = Recall, fill = model_name), stat = "identity", position = position_dodge2())+
@@ -41,7 +41,7 @@ ggplot(data = performance[1:8])+
   theme_bw()
 
 # binary predictors, selection + text
-ggplot(data = performance[1:8])+
+ggplot(data = rbind(performance[1:4], performance[6:8]))+
   geom_bar(mapping = aes(x = "Precision", y = Precision, fill = model_name), stat = "identity", position = position_dodge2())+
   geom_errorbar(mapping = aes(x = "Precision", ymin = Precision - SE_Precision, ymax = Precision + SE_Precision), position = position_dodge2())+
   geom_text(aes(x = "Precision", y = -0.02, label = round(Precision*100, 0)), position = position_dodge2(width = 0.9), size = 3.3)+
