b2fa8090c0eff070b721736e79ea1b13c6d340a7
Laura <ge72fev@mytum.de>
2022-11-27 13:06:36 +0100

    performance assessment plots

--- R_scripts/performance_viz.R
+++ R_scripts/performance_viz.R
@@ -208,11 +208,13 @@ ggplot(data = rbind(performance_t[7], performance_t[20], performance_t[8]))+
 ######### mobidb models ##############
 
 performance <- data.table(read.table("../results/logs/performance_assessment_mobidb.tsv", header = TRUE, sep = "\t"))
-performance <- cbind(performance, model_name = c("0 mobidb_CNN_0", "1 mobidb_CNN_1", "2 mobidb_FNN_0", "3 mmobidb_FNN_1", "4 mobidb_FNN_2", 
-                                                 "5 mobidib_D_CNN_0", "6 mobidb_D_CNN_1", "7 mobidib_D_FNN_0", "8 mobidib_D_FNN_1",
+performance <- cbind(performance, model_name = c("00 mobidb_CNN_0", "01 mobidb_CNN_1", "02 mobidb_CNN_2", 
+                                                 "03 mobidb_FNN_0", "04 mobidb_FNN_1", "05 mobidb_FNN_2", "06 mobidb_FNN_3", "07 mobidb_FNN_4", "08 mobidb_FNN_5", 
+                                                 "09 mobidib_D_CNN_0", "10 mobidb_D_CNN_1", "11 mobidb_D_CNN_2",
+                                                 "12 mobidib_D_FNN_0", "13 mobidib_D_FNN_1", "14 mobidb_D_FNN_2", "15 mobidb_D_FNN_3", "16 mobidb_D_FNN_4",
                                                  "random baseline", "random baseline disorder"))
 # whole protein prediction
-ggplot(data = rbind(performance[1:5], performance[10]))+
+ggplot(data = rbind(performance[1:9], performance[18]))+
   geom_bar(mapping = aes(x = "Precision", y = Precision, fill = model_name), stat = "identity", position = position_dodge2())+
   geom_errorbar(mapping = aes(x = "Precision", ymin = Precision - SE_Precision, ymax = Precision + SE_Precision), position = position_dodge2())+
   geom_bar(mapping = aes(x = "Recall", y = Recall, fill = model_name), stat = "identity", position = position_dodge2())+
@@ -229,12 +231,12 @@ ggplot(data = rbind(performance[1:5], performance[10]))+
   geom_errorbar(mapping = aes(x = "Neg_Recall", ymin = Neg_Recall - SE_Neg_Recall, ymax = Neg_Recall + SE_Neg_Recall), position = position_dodge2())+
   ylab("Value")+
   xlab("")+
-  ggtitle("Performance of MobiDB Predictors on the whole protein")+
+  ggtitle("Performance of MobiDB Predictors on the Whole Protein")+
   scale_fill_colorblind7()+
   theme_bw()
 
 # whole protein prediction, selection + text
-ggplot(data = rbind(performance[1:5], performance[10]))+
+ggplot(data = rbind(performance[1:9], performance[18]))+
   geom_bar(mapping = aes(x = "Precision", y = Precision, fill = model_name), stat = "identity", position = position_dodge2())+
   geom_errorbar(mapping = aes(x = "Precision", ymin = Precision - SE_Precision, ymax = Precision + SE_Precision), position = position_dodge2())+
   geom_text(aes(x = "Precision", y = -0.02, label = round(Precision*100, 0)), position = position_dodge2(width = 0.9), size = 3.3)+
@@ -249,13 +251,13 @@ ggplot(data = rbind(performance[1:5], performance[10]))+
   geom_text(aes(x = "MCC", y = -0.02, label = round(MCC*100, 0)), position = position_dodge2(width = 0.9), size = 3.3)+
   ylab("Value")+
   xlab("")+
-  ggtitle("Performance of MobiDB Predictors on the whole protein")+
+  ggtitle("Performance of MobiDB Predictors on the Whole Protein")+
   scale_fill_colorblind7()+
   theme_bw()
 
 
-# disorder only prediction, part 1
-ggplot(data = rbind(performance[1:5], performance[10:11]))+
+# disorder only prediction, part X
+ggplot(data = rbind(performance[2], performance[10], performance[13:17], performance[19]))+
   geom_bar(mapping = aes(x = "Precision", y = D.Precision, fill = model_name), stat = "identity", position = position_dodge2())+
   geom_errorbar(mapping = aes(x = "Precision", ymin = D.Precision - SE_D.Precision, ymax = D.Precision + SE_D.Precision), position = position_dodge2())+
   geom_bar(mapping = aes(x = "Recall", y = D.Recall, fill = model_name), stat = "identity", position = position_dodge2())+
@@ -272,12 +274,12 @@ ggplot(data = rbind(performance[1:5], performance[10:11]))+
   geom_errorbar(mapping = aes(x = "Neg_Recall", ymin = D.Neg_Recall - SE_D.Neg_Recall, ymax = D.Neg_Recall + SE_D.Neg_Recall), position = position_dodge2())+
   ylab("Value")+
   xlab("")+
-  ggtitle("Performance of MobiDB Predictors in disordered regions only, Part 1")+
+  ggtitle("Performance of MobiDB Predictors in Disordered Regions only, Part 4")+
   scale_fill_colorblind10()+
   theme_bw()
 
-# disorder only prediction, part 1, selection + text
-ggplot(data = rbind(performance[1:5], performance[10:11]))+
+# disorder only prediction, part X, selection + text
+ggplot(data = rbind(performance[2], performance[10], performance[13:17], performance[19]))+
   geom_bar(mapping = aes(x = "Precision", y = D.Precision, fill = model_name), stat = "identity", position = position_dodge2())+
   geom_errorbar(mapping = aes(x = "Precision", ymin = D.Precision - SE_D.Precision, ymax = D.Precision + SE_D.Precision), position = position_dodge2())+
   geom_text(aes(x = "Precision", y = -0.02, label = round(D.Precision*100, 0)), position = position_dodge2(width = 0.9), size = 3.3)+
@@ -295,52 +297,7 @@ ggplot(data = rbind(performance[1:5], performance[10:11]))+
   geom_text(aes(x = "MCC", y = -0.02, label = round(D.MCC*100, 0)), position = position_dodge2(width = 0.9), size = 3.3)+
   ylab("Value")+
   xlab("")+
-  ggtitle("Performance of MobiDB Predictors in disordered regions only, Part 1")+
+  ggtitle("Performance of MobiDB Predictors in disordered regions only, Part 4")+
   scale_fill_colorblind10()+
   theme_bw()
 
-
-# disorder only prediction, part 2
-ggplot(data = rbind(performance[2], performance[6:11]))+
-  geom_bar(mapping = aes(x = "Precision", y = D.Precision, fill = model_name), stat = "identity", position = position_dodge2())+
-  geom_errorbar(mapping = aes(x = "Precision", ymin = D.Precision - SE_D.Precision, ymax = D.Precision + SE_D.Precision), position = position_dodge2())+
-  geom_bar(mapping = aes(x = "Recall", y = D.Recall, fill = model_name), stat = "identity", position = position_dodge2())+
-  geom_errorbar(mapping = aes(x = "Recall", ymin = D.Recall - SE_D.Recall, ymax = D.Recall + SE_D.Recall), position = position_dodge2())+
-  geom_bar(mapping = aes(x = "Balanced.Acc.", y = D.Balanced.Acc., fill = model_name), stat = "identity", position = position_dodge2())+
-  geom_errorbar(mapping = aes(x = "Balanced.Acc.", ymin = D.Balanced.Acc. - SE_D.Balanced.Acc., ymax = D.Balanced.Acc. + SE_D.Balanced.Acc.), position = position_dodge2())+
-  geom_bar(mapping = aes(x = "F1", y = D.F1, fill = model_name), stat = "identity", position = position_dodge2())+
-  geom_errorbar(mapping = aes(x = "F1", ymin = D.F1 - SE_D.F1, ymax = D.F1 + SE_D.F1), position = position_dodge2())+
-  geom_bar(mapping = aes(x = "MCC", y = D.MCC, fill = model_name), stat = "identity", position = position_dodge2())+
-  geom_errorbar(mapping = aes(x = "MCC", ymin = D.MCC - SE_D.MCC, ymax = D.MCC + SE_D.MCC), position = position_dodge2())+
-  geom_bar(mapping = aes(x = "Neg_Precision", y = D.Neg_Precision, fill = model_name), stat = "identity", position = position_dodge2())+
-  geom_errorbar(mapping = aes(x = "Neg_Precision", ymin = D.Neg_Precision - SE_D.Neg_Precision, ymax = D.Neg_Precision + SE_D.Neg_Precision), position = position_dodge2())+
-  geom_bar(mapping = aes(x = "Neg_Recall", y = D.Neg_Recall, fill = model_name), stat = "identity", position = position_dodge2())+
-  geom_errorbar(mapping = aes(x = "Neg_Recall", ymin = D.Neg_Recall - SE_D.Neg_Recall, ymax = D.Neg_Recall + SE_D.Neg_Recall), position = position_dodge2())+
-  ylab("Value")+
-  xlab("")+
-  ggtitle("Performance of MobiDB Predictors in disordered regions only, Part 2")+
-  scale_fill_colorblind10()+
-  theme_bw()
-
-# disorder only prediction, part 2, selection + text
-ggplot(data = rbind(performance[2], performance[6:11]))+
-  geom_bar(mapping = aes(x = "Precision", y = D.Precision, fill = model_name), stat = "identity", position = position_dodge2())+
-  geom_errorbar(mapping = aes(x = "Precision", ymin = D.Precision - SE_D.Precision, ymax = D.Precision + SE_D.Precision), position = position_dodge2())+
-  geom_text(aes(x = "Precision", y = -0.02, label = round(D.Precision*100, 0)), position = position_dodge2(width = 0.9), size = 3.3)+
-  geom_bar(mapping = aes(x = "Recall", y = D.Recall, fill = model_name), stat = "identity", position = position_dodge2())+
-  geom_errorbar(mapping = aes(x = "Recall", ymin = D.Recall - SE_D.Recall, ymax = D.Recall + SE_D.Recall), position = position_dodge2())+
-  geom_text(aes(x = "Recall", y = -0.02, label = round(D.Recall*100, 0)), position = position_dodge2(width = 0.9), size = 3.3)+
-  geom_bar(mapping = aes(x = "Balanced.Acc.", y = D.Balanced.Acc., fill = model_name), stat = "identity", position = position_dodge2())+
-  geom_errorbar(mapping = aes(x = "Balanced.Acc.", ymin = D.Balanced.Acc. - SE_D.Balanced.Acc., ymax = D.Balanced.Acc. + SE_D.Balanced.Acc.), position = position_dodge2())+
-  geom_text(aes(x = "Balanced.Acc.", y = -0.02, label = round(D.Balanced.Acc.*100, 0)), position = position_dodge2(width = 0.9), size = 3.3)+
-  geom_bar(mapping = aes(x = "F1", y = D.F1, fill = model_name), stat = "identity", position = position_dodge2())+
-  geom_errorbar(mapping = aes(x = "F1", ymin = D.F1 - SE_D.F1, ymax = D.F1 + SE_D.F1), position = position_dodge2())+
-  geom_text(aes(x = "F1", y = -0.02, label = round(D.F1*100, 0)), position = position_dodge2(width = 0.9), size = 3.3)+
-  geom_bar(mapping = aes(x = "MCC", y = D.MCC, fill = model_name), stat = "identity", position = position_dodge2())+
-  geom_errorbar(mapping = aes(x = "MCC", ymin = D.MCC - SE_D.MCC, ymax = D.MCC + SE_D.MCC), position = position_dodge2())+
-  geom_text(aes(x = "MCC", y = -0.02, label = round(D.MCC*100, 0)), position = position_dodge2(width = 0.9), size = 3.3)+
-  ylab("Value")+
-  xlab("")+
-  ggtitle("Performance of MobiDB Predictors in disordered regions only, Part 2")+
-  scale_fill_colorblind10()+
-  theme_bw()
