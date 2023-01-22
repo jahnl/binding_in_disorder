8c421210104a64f70fc96cb65f9d4b19a2906d0b
Laura <ge72fev@mytum.de>
2023-01-22 14:46:58 +0100

    new predictions and visualizations

--- R_scripts/performance_viz.R
+++ R_scripts/performance_viz.R
@@ -210,14 +210,14 @@ ggplot(data = rbind(performance_t[7], performance_t[20], performance_t[8]))+
 performance <- data.table(read.table("../results/logs/performance_assessment_mobidb_new_balancing.tsv", header = TRUE, sep = "\t"))
 performance <- cbind(performance, model_name = c("00 mobidb_CNN_0", "01 mobidb_CNN_1", "02 mobidb_CNN_2", 
                                                  "03 mobidb_FNN_0", "04 mobidb_FNN_1", "05 mobidb_FNN_2", "06 mobidb_FNN_3", "07 mobidb_FNN_4", "08 mobidb_FNN_5", 
-                                                 "09 mobidib_D_CNN_0", "09.1 mobidib_D_CNN_0_lr0005", "09.2 mobidib_D_CNN_0_lr001", "09.3 mobidib_D_CNN_0_d2", "09.4 mobidib_D_CNN_0_d3", "09.5 mobidib_D_CNN_0_k3", "09.6 mobidib_D_CNN_0_k7", "09.7 mobidib_D_CNN_0_l8", 
+                                                 "09 mobidb_D_CNN_0", "09.1 mobidib_D_CNN_0_lr0005", "09.2 mobidib_D_CNN_0_lr001", "09.3 mobidib_D_CNN_0_d2", "09.4 mobidib_D_CNN_0_d3", "09.5 mobidib_D_CNN_0_k3", "09.6 mobidib_D_CNN_0_k7", "09.7 mobidib_D_CNN_0_l8", 
                                                  "10 mobidb_D_CNN_1", "11 mobidb_D_CNN_2",
                                                  "12 mobidib_D_FNN_0", "13 mobidib_D_FNN_1", "14 mobidb_D_FNN_2", "15 mobidb_D_FNN_3", "16 mobidb_D_FNN_4",
                                                  "AAindex_baseline", "AAindex_D_baseline", "random_baseline", 'random_D_baseline'))
 performance_test <- data.table(read.table("../results/logs/performance_assessment_test_mobidb_all_new.tsv", header = TRUE, sep = "\t"))
 performance_test <- cbind(performance_test, model_name = c("00 mobidb_CNN_0", "01 mobidb_CNN_1", "02 mobidb_CNN_2", 
                                                            "03 mobidb_FNN_0", "04 mobidb_FNN_1", "05 mobidb_FNN_2", "06 mobidb_FNN_3", "07 mobidb_FNN_4", "08 mobidb_FNN_5", 
-                                                           "09 mobidib_D_CNN_0", "09.1 mobidib_D_CNN_0_lr0005", "09.2 mobidib_D_CNN_0_lr001", "09.3 mobidib_D_CNN_0_d2", "09.4 mobidib_D_CNN_0_d3", "09.5 mobidib_D_CNN_0_k3", "09.6 mobidib_D_CNN_0_k7", "09.7 mobidib_D_CNN_0_l8", 
+                                                           "09 mobidb_D_CNN_0", "09.1 mobidib_D_CNN_0_lr0005", "09.2 mobidib_D_CNN_0_lr001", "09.3 mobidib_D_CNN_0_d2", "09.4 mobidib_D_CNN_0_d3", "09.5 mobidib_D_CNN_0_k3", "09.6 mobidib_D_CNN_0_k7", "09.7 mobidib_D_CNN_0_l8", 
                                                            "10 mobidb_D_CNN_1", "11 mobidb_D_CNN_2",
                                                            "12 mobidib_D_FNN_0", "13 mobidib_D_FNN_1", "14 mobidb_D_FNN_2", "15 mobidb_D_FNN_3", "16 mobidb_D_FNN_4",
                                                            "17 ANCHOR2", "18 DeepDISOBind",
@@ -351,3 +351,23 @@ ggplot(data = rbind(performance_test[1], performance_test[6], performance_test[1
   theme_bw()
 
 
+
+# prediction: selection,  val and Test difference
+val_perf <- data.table(rbind(performance[1], performance[6], performance[10], performance[22], performance[25:26], performance[28], use.names=FALSE))
+val_perf[] <- lapply(val_perf, function(x) as.numeric(x) )
+val_perf$model_name <- c("00 mobidb_CNN_0", "05 mobidb_FNN_2", "09 mobidb_D_CNN_0", "14 mobidb_D_FNN_2", "AAindex_baseline", "AAindex_D_baseline", "random_D_baseline")
+ggplot(data = rbind(performance_test[1], performance_test[6], performance_test[10], performance_test[22], performance_test[27:28], performance_test[30]))+
+  geom_bar(mapping = aes(x = "Balanced.Acc.", y = D.Balanced.Acc., fill = model_name), stat = "identity", position = position_dodge2(), alpha = 0.7)+
+  geom_errorbar(mapping = aes(x = "Balanced.Acc.", ymin = D.Balanced.Acc. - SE_D.Balanced.Acc., ymax = D.Balanced.Acc. + SE_D.Balanced.Acc.), position = position_dodge2())+
+  geom_bar(mapping = aes(x = "MCC", y = D.MCC, fill = model_name), stat = "identity", position = position_dodge2(), alpha = 0.7)+
+  geom_errorbar(mapping = aes(x = "MCC", ymin = D.MCC - SE_D.MCC, ymax = D.MCC + SE_D.MCC), position = position_dodge2())+
+  # val
+  geom_bar(data = val_perf, mapping = aes(x = "Balanced.Acc.", y = D.Balanced.Acc., fill = model_name), stat = "identity", position = position_dodge2(), alpha = 0.7)+
+  geom_errorbar(data = val_perf, mapping = aes(x = "Balanced.Acc.", ymin = D.Balanced.Acc. - SE_D.Balanced.Acc., ymax = D.Balanced.Acc. + SE_D.Balanced.Acc.), position = position_dodge2())+
+  geom_bar(data = val_perf, mapping = aes(x = "MCC", y = D.MCC, fill = model_name), stat = "identity", position = position_dodge2(), alpha = 0.7)+
+  geom_errorbar(data = val_perf, mapping = aes(x = "MCC", ymin = D.MCC - SE_D.MCC, ymax = D.MCC + SE_D.MCC), position = position_dodge2())+
+  ylab("Value")+
+  xlab("")+
+  ggtitle("Difference of the Performance Between Validation and Test Set")+
+  scale_fill_colorblind10()+
+  theme_bw()
