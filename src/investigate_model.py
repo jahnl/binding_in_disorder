0aa296e7c868d1f2cd8f85dc1680546b7e4709fe
Laura <ge72fev@mytum.de>
2024-05-07 22:06:33 +0200

    handle missing attribute in embeddings file while predicting

--- src/investigate_model.py
+++ src/investigate_model.py
@@ -851,7 +851,11 @@ def predict(train_embeddings: str, dataset_dir: str, test_embeddings: str, annot
         aaindex = False
         with h5py.File(embeddings_in, 'r') as f:
             for key, embedding in f.items():
-                original_id = embedding.attrs['original_id']
+                try:
+                    original_id = embedding.attrs['original_id']
+                except KeyError:
+                    # in some embeddings (e.g. ESM-2) there is no original_id attribute, additionally shorten the ID like for ProtT5
+                    original_id = key.split(' ')[0]
                 embeddings[original_id] = np.array(embedding)
         # now {IDs: embeddings} are written in the embeddings dictionary
     else:
